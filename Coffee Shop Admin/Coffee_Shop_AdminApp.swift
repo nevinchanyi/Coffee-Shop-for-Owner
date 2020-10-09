558d5c628b95a547bc01865bc1e5d31bbd3441a6
Kostya Nevinchanyi <constantinekos9@gmail.com>
2020-10-09 15:50:13 +0300

    Initial Commit

---
+++ Coffee Shop Admin/Coffee_Shop_AdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coffee_Shop_AdminApp.swift
+//  Coffee Shop Admin
+//
+//  Created by constantine kos on 09.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct Coffee_Shop_AdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Coffee Shop Admin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Coffee Shop Admin
+//
+//  Created by constantine kos on 09.10.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
